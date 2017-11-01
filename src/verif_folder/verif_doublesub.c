9cde2238f0cb8705931ac8addc829b2a8ad5e775
valentin.dewez <valentin.dewez@epitech.eu>
2017-11-01 19:11:29 +0100

    adding verif double sub , it isn'ttest yet, i'm gonna check it later

---
+++ src/verif_folder/verif_doublesub.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2017
+** verif_doublesub.c
+** File description:
+** verif double sub
+*/
+
+char	*OPS = "()+-*/%";
+
+int	verif_double_sub(char *str)
+{
+	int	i = 0;
+
+	while (str[i]) {
+		if (str[i] == OPS(OP_SUB_IDX) && str[i + 1] == OPS(OP_SUB_IDX)) {
+			my_putstr(SYNTAX_ERROR_MSG);
+			exit(EXIT_SYNTAX);
+		}
+		i++;
+	}
+	return (0);
+}
