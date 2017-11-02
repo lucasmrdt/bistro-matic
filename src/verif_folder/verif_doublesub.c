9d19f4e48a722fd8b4e7c7b68d80cc62ee19d778
valentin.dewez <valentin.dewez@epitech.eu>
2017-11-02 10:25:31 +0100

    verif_sub

--- src/verif_folder/verif_doublesub.c
+++ src/verif_folder/verif_doublesub.c
@@ -5,6 +5,10 @@
 ** verif double sub
 */
 
+#include "my.h"
+#include "bistromatic.h"
+#include <stdlib.h>
+
 char	*OPS = "()+-*/%";
 
 int	verif_double_sub(char *str)
@@ -12,7 +16,7 @@ int	verif_double_sub(char *str)
 	int	i = 0;
 
 	while (str[i]) {
-		if (str[i] == OPS(OP_SUB_IDX) && str[i + 1] == OPS(OP_SUB_IDX)) {
+		if (str[i] == OPS[OP_SUB_IDX] && str[i + 1] == OPS[OP_SUB_IDX]) {
 			my_putstr(SYNTAX_ERROR_MSG);
 			exit(EXIT_SYNTAX);
 		}
